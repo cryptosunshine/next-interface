50bf95bfb921e6e7ec39068e0bd388d6f297755f
Shine <Shine>
2023-08-08 14:47:10 +0800

    add compression-webpack-plugin, fallback fs...

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withLess = require("next-with-less");
+const CompressionPlugin = require('compression-webpack-plugin');
 
 const nextConfig = withLess({
     reactStrictMode: true,
@@ -20,11 +21,21 @@ const nextConfig = withLess({
         '/home': { page: '/home' },
       }
     },
-    webpack: {
-      module: {
-        rules: []
-      },
-    }
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      // 生产环境下启用Gzip压缩
+      if (!dev) {
+        config.plugins.push(new CompressionPlugin());
+      }
+      config.resolve.fallback = { fs: false, net: false, tls: false };
+      // 在这里进行字体加载的配置
+      config.module.rules.push({
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      });
+      return config;
+    },
   })
   
   module.exports = nextConfig
